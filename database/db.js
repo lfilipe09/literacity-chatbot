3ed223ded330099ec3f92f267d3aa76602e1b0a0
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-03-27 18:47:00 -0300

    fix: ssl update

--- database/db.js
+++ database/db.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   dialect: 'postgres',
+  protocol: 'postgres',
   native: true,
   ssl: true,
   dialectOptions: {
-    ssl: true
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   },
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT
